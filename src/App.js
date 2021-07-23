560aeab0bcb205412d4ebe098ddd863c832b731e
Georjane <pzgeorjane@gmail.com>
2021-07-23 06:23:25 +0100

    Initialized repo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Magic Bookstore</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
