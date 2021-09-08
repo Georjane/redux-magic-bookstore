677b2faa66ac8d555940bd0c7729324ab22adc5a
Georjane <pzgeorjane@gmail.com>
2021-09-08 11:46:21 +0100

    Add home and login components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,75 @@
-import BooksForm from '../containers/BooksForm';
-import BooksList from '../containers/BooksList';
+import React, { Component } from 'react';
+import axios from 'axios'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoggedIn: false,
+      user: {}
+     };
+  };
 
-function App() {
-  return (
-    <div className="root">
-      <main>
-        <BooksList />
-        <hr />
-        <BooksForm />
-      </main>
-    </div>
-  );
-}
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+handleLogout = () => {
+    this.setState({
+    isLoggedIn: false,
+    user: {}
+    })
+  }
 
+  loginStatus = () => {
+    axios.get('http://localhost:3000/auth/login', 
+   {withCredentials: true})    
+.then(response => {
+  console.log(response);
+      // if (response.data.logged_in) {
+      //   this.handleLogin(response)
+      // } else {
+      //   this.handleLogout()
+      // }
+    })
+    .catch(error => console.log('api errors:', error))
+  };
+
+  componentDidMount() {
+    this.loginStatus()
+  }
+
+render() {
+    return (
+      <div>
+         <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={}/>
+            <Route exact path='/login' component={}/>
+            <Route exact path='/signup' component={}/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+};
 export default App;
+
+// import BooksForm from '../containers/BooksForm';
+// import BooksList from '../containers/BooksList';
+
+// function App() {
+//   return (
+//     <div className="root">
+//       <main>
+//         <BooksList />
+//         <hr />
+//         <BooksForm />
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      errors: '',
+    };
+  }
+
+handleChange = (event) => {
+  const { name, value } = event.target;
+  this.setState({
+    [name]: value,
+  });
+};
+
+handleSubmit = (event) => {
+  event.preventDefault();
+};
+
+render() {
+  const { username, password } = this.statereturn(
+    <div>
+      <h1>Log In</h1>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          placeholder="username"
+          type="text"
+          name="username"
+          value={username}
+          onChange={this.handleChange}
+        />
+        <input
+          placeholder="password"
+          type="password"
+          name="password"
+          value={password}
+          onChange={this.handleChange}
+        />
+        <button placeholder="submit" type="submit">
+          Log In
+        </button>
+        <div>
+          or
+          {' '}
+          <Link to="/signup">sign up</Link>
+        </div>
+
+      </form>
+    </div>,
+  );
+}
+}
+export default Login;
